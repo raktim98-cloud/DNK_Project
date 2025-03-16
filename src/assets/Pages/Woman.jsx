5555e174760659d3b5d3c060821a52b6542d691c
Raktim Shuvrow <shuvrow98@gmail.com>
2025-03-17 01:15:16 +0600

    eoman page set

--- src/assets/Pages/Woman.jsx
+++ src/assets/Pages/Woman.jsx
@@ -1,8 +1,31 @@
-import Nav from "../Components/Nav"
+import { FaGreaterThan } from "react-icons/fa6";
 
 function Woman() {
   return (
-    <div> Woman</div>
+    <section className="py-20 bg-[#F5F7F9]">
+      <div className="containe mx-auto">
+        <div className="grid grid-cols-[400px_1fr] px-5">
+          <div className="left">
+            <form action="#" className="flex items-center gap-1.5">
+              <input className="px-[12px] py-[8px] border border-[#e2e2e2;] focus:border-black/50 focus:outline-dotted transition-colors bg-white" type="search" name="search" id="search" placeholder="Search Products..." />
+              <button type="submit" className="px-2 py-[12px] bg-[#0084d6;] text-white"><FaGreaterThan /></button>
+            </form>
+            <div className="filter_price">
+
+            </div>
+            <div className="catagories">
+
+            </div>
+            <div className="best_seller">
+
+            </div>
+          </div>
+          <div className="product_part">
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
